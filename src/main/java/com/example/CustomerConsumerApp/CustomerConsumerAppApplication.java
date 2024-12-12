268dd0c085f9bd68aab8a153d52d9096be4a37f5
Harini Sathya <harinisathya17@gmail.com>
2024-12-11 19:16:56 -0600

    Added method for Delete and added logs instead of SysOut

--- src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
+++ src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
@@ -1,6 +1,8 @@
 package com.example.CustomerConsumerApp;
 
 import com.example.CustomerConsumerApp.entity.CustomerEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +19,9 @@ import java.util.Optional;
 
 @SpringBootApplication
 public class CustomerConsumerAppApplication implements CommandLineRunner {
+
+		private static final Logger logger = LoggerFactory.getLogger(CustomerConsumerAppApplication.class);
+
 		private static final String BASE_URL = "http://localhost:8080/customer-service";
 		public static void main(String[] args) {
 		SpringApplication.run(CustomerConsumerAppApplication.class, args);
@@ -32,19 +37,24 @@ public class CustomerConsumerAppApplication implements CommandLineRunner {
 		getCustomerById(String.valueOf(4L));
 		getAllCustomers();
 		updateCustomer(new CustomerEntity(5L, "Harini ", "Sathya", "", "harini.sathyanarayanan@example.com", "8327252110"));
-
+		deleteCustomer(String.valueOf(9L));
 	}
 
 
 	public void createCustomer(CustomerEntity customer) {
+		logger.info("Inside Create Customer Method");
+
 		String url = BASE_URL + "/save";
 		ResponseEntity<CustomerEntity> response = restTemplate.postForEntity(url, customer, CustomerEntity.class);
-		System.out.println("Created Customer: " + response.getBody().toString());
+
+		logger.info("Created Customer: " + response.getBody().toString());
 	}
 
 	public void getCustomerById(String customerId) {
-		String url = BASE_URL + "/getCustomer";
 
+		logger.info("Inside Get Customer By Id Method");
+
+		String url = BASE_URL + "/getCustomer";
 		Map<String, String> params = new HashMap<>();
 		params.put("id", customerId);
 
@@ -59,25 +69,33 @@ public class CustomerConsumerAppApplication implements CommandLineRunner {
 
 		Optional<CustomerEntity> customer = response.getBody();
 		if (customer.isPresent()) {
-			System.out.println("Fetched Customer: " + customer.get().toString());
+			logger.info("Fetched Customer: " + customer.get().toString());
 		} else {
-			System.out.println("Customer not found for ID: " + customerId);
+			logger.info("Customer not found for ID: " + customerId);
 		}
 	}
 
 	public void getAllCustomers() {
+
+		logger.info("Inside Get All Customers Method");
+
 		String url = BASE_URL + "/getAllCustomers";
 		ResponseEntity<CustomerEntity[]> response = restTemplate.getForEntity(url, CustomerEntity[].class);
 		CustomerEntity[] customers = response.getBody();
-		System.out.println("All Customers: ");
+
+		logger.info("All Customers: ");
+
 		for (CustomerEntity customer : customers) {
-			System.out.println(customer.toString());
+			logger.info(customer.toString());
 		}
 	}
 
 	public void updateCustomer(CustomerEntity customer) {
 
+		logger.info("Inside Update Customer Method");
+
 		String url = BASE_URL + "/updateCustomer";
+
 		try{
 			ResponseEntity<CustomerEntity> response = restTemplate.exchange(
 					url,
@@ -86,17 +104,38 @@ public class CustomerConsumerAppApplication implements CommandLineRunner {
 					CustomerEntity.class
 			);
 			if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
-				System.out.println("Updated Customer: " + response.getBody().toString());
+				logger.info("Updated Customer: " + response.getBody().toString());
 			} else {
-				System.out.println("Failed to update Customer with id" + customer.getId() );
+				logger.info("Failed to update Customer with id" + customer.getId() );
 			}
 		} catch (HttpClientErrorException.NotFound e) {
-			System.out.println("Customer with ID " + customer.getId() + " not found.");
+			logger.info("Customer with ID " + customer.getId() + " not found.");
 		} catch (HttpClientErrorException e) {
-			System.out.println("Failed to update Customer. Error: " + e.getStatusCode() + " - " + e.getMessage());
+			logger.info("Failed to update Customer. Error: " + e.getStatusCode() + " - " + e.getMessage());
 		}
 	}
 
+	public void deleteCustomer(String customerId) {
 
+		logger.info("Inside Delete Customer Method");
 
+		String url = BASE_URL + "/deleteCustomer";
+		Map<String, String> params = new HashMap<>();
+		params.put("id", customerId);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+				url + "?id={id}",
+				HttpMethod.DELETE,
+				null,
+				new ParameterizedTypeReference<String>() {
+				},
+				params
+		);
+
+		if (response.getStatusCode().is2xxSuccessful()) {
+			logger.info(response.getBody());
+		} else {
+			logger.info("Failed to delete customer with ID: " + customerId);
+		}
+	}
 }
