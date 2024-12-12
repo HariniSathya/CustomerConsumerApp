d894abbd01d7ef83a0ebed3f9214c4d0f3e16dbd
Harini Sathya <harinisathya17@gmail.com>
2024-12-11 18:52:11 -0600

    Added methods for getting customer details from Customer API

--- src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
+++ src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
@@ -4,8 +4,14 @@ import com.example.CustomerConsumerApp.entity.CustomerEntity;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 @SpringBootApplication
 public class CustomerConsumerAppApplication implements CommandLineRunner {
@@ -21,6 +27,8 @@ public class CustomerConsumerAppApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// Test the API methods
 		createCustomer(new CustomerEntity("Allen ", "", "Shaw", "Allen.shaw@email.com", "9876543210"));
+		getCustomerById(String.valueOf(4L));
+		getAllCustomers();
 	}
 
 
@@ -30,6 +38,38 @@ public class CustomerConsumerAppApplication implements CommandLineRunner {
 		System.out.println("Created Customer: " + response.getBody().toString());
 	}
 
+	public void getCustomerById(String customerId) {
+		String url = BASE_URL + "/getCustomer";
+
+		Map<String, String> params = new HashMap<>();
+		params.put("id", customerId);
+
+		ResponseEntity<Optional<CustomerEntity>> response = restTemplate.exchange(
+				url + "?id={id}",
+				HttpMethod.GET,
+				null,
+				new ParameterizedTypeReference<Optional<CustomerEntity>>() {
+				},
+				params
+		);
+
+		Optional<CustomerEntity> customer = response.getBody();
+		if (customer.isPresent()) {
+			System.out.println("Fetched Customer: " + customer.get().toString());
+		} else {
+			System.out.println("Customer not found for ID: " + customerId);
+		}
+	}
+
+	public void getAllCustomers() {
+		String url = BASE_URL + "/getAllCustomers";
+		ResponseEntity<CustomerEntity[]> response = restTemplate.getForEntity(url, CustomerEntity[].class);
+		CustomerEntity[] customers = response.getBody();
+		System.out.println("All Customers: ");
+		for (CustomerEntity customer : customers) {
+			System.out.println(customer.toString());
+		}
+	}
 
 
 }
