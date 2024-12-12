6fbebb976725b922d8b4e3dd303cc6ed2dbac072
Your Name <harinisathya17@gmail.com>
2024-12-11 18:25:42 -0600

    Initial commit

---
+++ src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CustomerConsumerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerConsumerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerConsumerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomerConsumerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerConsumerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
