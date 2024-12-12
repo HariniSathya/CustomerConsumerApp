08cc0b8d5fd203ef64e38b91f988f07e13e6c6b0
Harini Sathya <harinisathya17@gmail.com>
2024-12-11 18:41:20 -0600

    Created Customer Entity and added method to create Customer

--- src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
+++ src/main/java/com/example/CustomerConsumerApp/CustomerConsumerAppApplication.java
@@ -1,13 +1,35 @@
 package com.example.CustomerConsumerApp;
 
+import com.example.CustomerConsumerApp.entity.CustomerEntity;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class CustomerConsumerAppApplication {
-
-	public static void main(String[] args) {
+public class CustomerConsumerAppApplication implements CommandLineRunner {
+		private static final String BASE_URL = "http://localhost:8080/customer-service";
+		public static void main(String[] args) {
 		SpringApplication.run(CustomerConsumerAppApplication.class, args);
+		}
+
+		private RestTemplate restTemplate = new RestTemplate();
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Test the API methods
+		createCustomer(new CustomerEntity("Allen ", "", "Shaw", "Allen.shaw@email.com", "9876543210"));
 	}
+
+
+	public void createCustomer(CustomerEntity customer) {
+		String url = BASE_URL + "/save";
+		ResponseEntity<CustomerEntity> response = restTemplate.postForEntity(url, customer, CustomerEntity.class);
+		System.out.println("Created Customer: " + response.getBody().toString());
+	}
+
+
 
 }
---
+++ src/main/java/com/example/CustomerConsumerApp/entity/CustomerEntity.java
@@ -0,0 +1,94 @@
+package com.example.CustomerConsumerApp.entity;
+public class CustomerEntity {
+    private Long id;
+    private String firstName;
+
+    private String middleName;
+
+    private String lastName;
+
+    private String emailAddress;
+
+    private String phoneNumber;
+
+    public CustomerEntity(Long id, String firstName, String lastName, String middleName, String emailAddress, String phoneNumber) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public CustomerEntity() {
+    }
+
+    public CustomerEntity(String firstName, String middleName, String lastName, String emailAddress, String phoneNumber) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerEntity{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
+
